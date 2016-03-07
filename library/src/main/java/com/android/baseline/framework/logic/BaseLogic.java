355b238fef419d1a39eb24274c1f2e675971ebb7
hiphonezhu <hiphonezhu@gmail.com>
2016-03-07 14:11:55 +0800

    BaseLogic support custom RequestQueue, and can be switched.

--- library/src/main/java/com/android/baseline/framework/logic/BaseLogic.java
+++ library/src/main/java/com/android/baseline/framework/logic/BaseLogic.java
@@ -1,13 +1,7 @@
 package com.android.baseline.framework.logic;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ConcurrentLinkedQueue;
-
 import android.os.Message;
+import android.text.TextUtils;
 
 import com.android.baseline.AppDroid;
 import com.android.baseline.framework.volley.InfoResultRequest;
@@ -15,19 +9,29 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import de.greenrobot.event.EventBus;
 
 /**
  * 1、统一的Volley请求队列以及Request的发送
  * 2、使用EventBus通讯订阅者与发送者
  * 3、提供订阅者注册、解绑和业务请求结果统一分发的实现
+ * 4、支持设置不同的RequestQueue, 且可以动态切换Volley队列
  * [
  *   一、针对当前框架做了修改
  *       每一个BaseLogic对象默认情况下使用不同的EventBus{@link #BaseLogic(Object subscriber)}, 
  *       保证同一种类型的事件不会发送给多个订阅者(EventBus默认情况会发送个多个订阅者)
  *     
  *   二、如果需要同一类型的事件发送给多个订阅者
- *       使用{@link #BaseLogic(Object subscriber, EventBus eventBus)}构造函数, 为每个订阅者提供同一个EventBus对象(例如EventBus.getDefault()), 
+ *       使用{@link #BaseLogic(Object subscriber, EventBus eventBus, String volleyTag)}构造函数, 为每个订阅者提供同一个EventBus对象(例如EventBus.getDefault()),
  *       这样会出现以下情况
  *       1、msg的what相同的情况会被多个订阅者接受并处理
  *       2、msg的what不相同的话会被多个订阅者接受, 但不会被处理(具体需要业务层控制)
@@ -39,8 +43,8 @@ public class BaseLogic implements ILogic
 {
     // 存储所有的订阅者
     private List<Object> subscribers = new ArrayList<Object>();
-    // Volley请求队列
-    private static RequestQueue requestQueue = Volley.newRequestQueue(AppDroid
+    // 默认Volley请求队列
+    private final static RequestQueue DEFAULT_REQUEST_QUEUE = Volley.newRequestQueue(AppDroid
             .getInstance().getApplicationContext());
     // Default EventBus
     private EventBus mEventBus;
@@ -52,21 +56,52 @@ public class BaseLogic implements ILogic
     private static boolean isSyncRequesting; // 是否正在执行同步请求
     private static int syncRequestId = -1000; // 同步请求Id
     private static ConcurrentLinkedQueue<Request<?>> waitQueue = new ConcurrentLinkedQueue<Request<?>>(); // 缓存后续的请求
+
+    // Volley队列池
+    private static Map<String, RequestQueue> requestQueuePool = new HashMap<String, RequestQueue>();
+    // 默认Volley tag
+    public static final String DEFAULT_VOLLEY_TAG = "default";
+    static
+    {
+        requestQueuePool.put(DEFAULT_VOLLEY_TAG, DEFAULT_REQUEST_QUEUE);
+    }
+    // 当前队列标识
+    private static String currentVolleyTag = DEFAULT_VOLLEY_TAG;
+    // 当前Volley队列
+    private RequestQueue currentRequestQueue;
+
     /**
      * Constructor with a subscriber
      * @param subscriber
      */
     public BaseLogic(Object subscriber)
     {
-        this(subscriber, new EventBus());
+        this(subscriber, new EventBus(), currentVolleyTag);
     }
-    
+
     /**
-     * Constructor with custom EventBus
-     * @param eventBus
+     * Constructor with a subscriber and custom volleyTag
+     * @param subscriber
+     * @param volleyTag
      */
-    public BaseLogic(Object subscriber, EventBus eventBus)
+    public BaseLogic(Object subscriber, String volleyTag)
     {
+        this(subscriber, new EventBus(), volleyTag);
+    }
+
+    /**
+     * Constructor with custom EventBus and volleyTag
+     * @param eventBus
+     * @param volleyTag
+     */
+    public BaseLogic(Object subscriber, EventBus eventBus, String volleyTag)
+    {
+        if (TextUtils.isEmpty(volleyTag))
+        {
+            volleyTag = currentVolleyTag;
+        }
+        currentRequestQueue = requestQueuePool.get(volleyTag);
+
         if (eventBus == null)
         {
             mEventBus = EventBus.getDefault();
@@ -77,7 +112,44 @@ public class BaseLogic implements ILogic
         }
         register(subscriber);
     }
-    
+
+    /**
+     * 添加请求队列
+     * @param volleyTag
+     * @param requestQueue
+     */
+    public static void injectVolleyQueue(String volleyTag, RequestQueue requestQueue)
+    {
+        if (TextUtils.isEmpty(volleyTag))
+        {
+            throw new RuntimeException("volleyTag can not be null!");
+        }
+        if (requestQueue == null)
+        {
+            throw new RuntimeException("requestQueue can not be null!");
+        }
+
+        requestQueuePool.put(volleyTag, requestQueue);
+    }
+
+    /**
+     * 设置默认队列
+     * @param volleyTag
+     */
+    public static void setDefaultVolleyQueue(String volleyTag)
+    {
+        if (TextUtils.isEmpty(volleyTag))
+        {
+            throw new RuntimeException("volleyTag can not be null!");
+        }
+        if (requestQueuePool.get(volleyTag) == null)
+        {
+            throw new RuntimeException("can not find RequestQueue with volleyTag: " + volleyTag);
+        }
+
+        currentVolleyTag = volleyTag;
+    }
+
     @Override
     public void register(Object subscriber)
     {
@@ -115,7 +187,7 @@ public class BaseLogic implements ILogic
     @Override
     public void cancel(Object tag)
     {
-        requestQueue.cancelAll(tag);
+        currentRequestQueue.cancelAll(tag);
     }
     
     /**
@@ -138,7 +210,7 @@ public class BaseLogic implements ILogic
      */
     protected <T> void sendRequest(Request<T> request)
     {
-        requestQueue.add(request);
+        currentRequestQueue.add(request);
     }
     
     /**
@@ -149,22 +221,38 @@ public class BaseLogic implements ILogic
      */
     protected <T> void sendRequest(Request<T> request, Object tag)
     {
-        sendRequest(request, tag, requestQueue);
+        sendRequest(request, tag, currentVolleyTag);
     }
-    
+
     /**
-     * 使用自定义队列发送网络请求, 并给这个请求设置TAG
+     * 发送网络请求, 并给这个请求设置TAG
      * @param <T>
      * @param request
      * @param tag
-     * @param requestQueue
+     * @param volleyTag 请求队列标识
      */
-    protected <T> void sendRequest(Request<T> request, Object tag, RequestQueue requestQueue)
+    protected <T> void sendRequest(Request<T> request, Object tag, String volleyTag)
     {
         if (tag != null)
         {
             request.setTag(tag);
         }
+
+        // 校验队列是否存在
+        if (TextUtils.isEmpty(volleyTag))
+        {
+            volleyTag = currentVolleyTag;
+        }
+
+        currentRequestQueue = requestQueuePool.get(volleyTag);
+        if (currentRequestQueue == null)
+        {
+            throw new RuntimeException("can not find RequestQueue with volleyTag: " + volleyTag);
+        }
+
+        // 记录使用的请求队列
+        request.setVolleyTag(volleyTag);
+
         synchronized (lock)
         {
             if (isSyncRequesting)
@@ -183,20 +271,9 @@ public class BaseLogic implements ILogic
                     }
                 }
                 requestTags.add(tag);
-                requestQueue.add(request);
+                currentRequestQueue.add(request);
             }
         }
-    }
-    
-    /**
-     * 使用自定义队列发送网络请求
-     * @param <T>
-     * @param request
-     * @param requestQueue
-     */
-    protected <T> void sendRequest(Request<T> request, RequestQueue requestQueue)
-    {
-        sendRequest(request, null, requestQueue);
     }
 
     /**
@@ -240,7 +317,8 @@ public class BaseLogic implements ILogic
                     
                     // 加入到请求队列
                     requestTags.add(request.getTag());
-                    requestQueue.add(request);
+                    // 找出对应的请求队列
+                    requestQueuePool.get(request.getVolleyTag()).add(request);
                 }
             }
         }
