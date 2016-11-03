dbf6e70dfc33863514a48a3754bd10a395337206
hiphonezhu <hiphonezhu@gmail.com>
2016-11-03 16:45:50 +0800

    支持https双向验证。

--- library/src/main/java/com/android/baseline/framework/logic/net/SSLFactory.java
+++ library/src/main/java/com/android/baseline/framework/logic/net/SSLFactory.java
@@ -9,6 +9,7 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
@@ -16,6 +17,8 @@ import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
@@ -33,7 +36,7 @@ public class SSLFactory {
      * 创建SSLSocketFactory
      *
      * @param context
-     * @param certRawResIds ca证书raw资源数组
+     * @param certRawResIds 包含公钥的cer证书
      * @return
      */
     public static SSLSocketFactory build(Context context, int... certRawResIds) {
@@ -56,10 +59,62 @@ public class SSLFactory {
     }
 
     /**
+     * 创建SSLSocketFactory(双向认证)
+     *
+     * @param context
+     * @param bksRawResIds jks转化之后的bks格式证书(转化方式: https://sourceforge.net/projects/portecle/files/latest/download?source=files下载portecle-1.9.zip)
+     * @param pwd 证书的秘钥
+     * @param certRawResIds 包含公钥的cer证书
+     * @return
+     */
+    public static SSLSocketFactory buildWithClientAuth(Context context, int bksRawResIds, String pwd, int... certRawResIds) {
+        try {
+            KeyStore keyStore = buildKeyStore(context, certRawResIds);
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keyStore);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(buildClient(context, bksRawResIds, pwd), tmf.getTrustManagers(), new SecureRandom());
+            return sslContext.getSocketFactory();
+        } catch (KeyStoreException e1) {
+        } catch (CertificateException e2) {
+        } catch (NoSuchAlgorithmException e3) {
+        } catch (IOException e4) {
+        } catch (KeyManagementException e5) {
+        }
+        return null;
+    }
+
+    private static KeyManager[] buildClient(Context context, int bksRawResIds, String pwd)
+    {
+        try {
+            KeyStore clientKeyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            // jks证书需要转化为bks格式
+            clientKeyStore.load(context.getResources().openRawResource(bksRawResIds), pwd.toCharArray());
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            keyManagerFactory.init(clientKeyStore, pwd.toCharArray());
+            return keyManagerFactory.getKeyManagers();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
      * 创建SSLSocketFactory(信任所有https)
      *
      * @return
      */
+    @SuppressWarnings("强烈不建议使用, https形同虚设")
     public static SSLSocketFactory build() {
         SSLContext sslContext = null;
         try {
