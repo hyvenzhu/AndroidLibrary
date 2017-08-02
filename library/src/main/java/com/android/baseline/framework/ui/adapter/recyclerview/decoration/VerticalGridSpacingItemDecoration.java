350db1cc68da7fe475b7dab5646a6c20e161047b
zhuhf <zhuhf@jcgroup.com.cn>
2017-08-02 18:16:24 +0800

    优化 GridLayoutManager 间距方案；

--- library/src/main/java/com/android/baseline/framework/ui/adapter/recyclerview/decoration/VerticalGridSpacingItemDecoration.java
+++ library/src/main/java/com/android/baseline/framework/ui/adapter/recyclerview/decoration/VerticalGridSpacingItemDecoration.java
@@ -1,6 +1,7 @@
 package com.android.baseline.framework.ui.adapter.recyclerview.decoration;
 
 import android.graphics.Rect;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -11,34 +12,36 @@ import android.view.View;
  */
 public class VerticalGridSpacingItemDecoration extends RecyclerView.ItemDecoration {
     
-    private int spanCount;
     private int spacing;
     private boolean includeEdge;
     
-    public VerticalGridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-        this.spanCount = spanCount;
+    public VerticalGridSpacingItemDecoration(int spacing, boolean includeEdge) {
         this.spacing = spacing;
         this.includeEdge = includeEdge;
     }
     
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-        int position = parent.getChildAdapterPosition(view); // item position
-        int column = position % spanCount; // item column
-        
-        if (includeEdge) {
-            outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-            outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+        if (parent.getLayoutManager() instanceof GridLayoutManager) {
+            GridLayoutManager gridLayoutManager = (GridLayoutManager) parent.getLayoutManager();
+            int spanCount = gridLayoutManager.getSpanCount();
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
             
-            if (position < spanCount) { // top edge
-                outRect.top = spacing;
-            }
-            outRect.bottom = spacing; // item bottom
-        } else {
-            outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-            outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-            if (position >= spanCount) {
-                outRect.top = spacing; // item top
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+                
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / 2; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / 2; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
             }
         }
     }
