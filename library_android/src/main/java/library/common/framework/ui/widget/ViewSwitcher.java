e00f89c2aa33c0a3339639780d827621ad3f3b15
jufeng <hiphonezhu@gmail.com>
2020-07-22 15:08:42 +0800

    修复 Loading 视图问题；

--- library_android/src/main/java/library/common/framework/ui/widget/ViewSwitcher.java
+++ library_android/src/main/java/library/common/framework/ui/widget/ViewSwitcher.java
@@ -1,11 +1,8 @@
 package library.common.framework.ui.widget;
 
-import android.graphics.Rect;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
-import android.widget.FrameLayout;
 
 /**
  * 两个 View 切换显示
@@ -22,7 +19,6 @@ public class ViewSwitcher {
     private int viewIndex;
     private ViewGroup.LayoutParams params;
     int height;
-    int statusBarHeight;
 
     /**
      * 加载布局
@@ -36,26 +32,7 @@ public class ViewSwitcher {
         super();
         mView = view;
         init();
-        mView.getViewTreeObserver().addOnGlobalLayoutListener(globalLayoutListener);
-        int resourceId = view.getContext().getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            statusBarHeight = view.getContext().getResources().getDimensionPixelSize(resourceId);
-        }
     }
-
-    ViewTreeObserver.OnGlobalLayoutListener globalLayoutListener = new ViewTreeObserver.OnGlobalLayoutListener() {
-        @Override
-        public void onGlobalLayout() {
-            Rect rect = new Rect();
-            mView.getLocalVisibleRect(rect);
-            int height = rect.height();
-            if (null != loadingView && loadingView != mView && height != 0) {
-                ViewGroup.LayoutParams layoutParams = loadingView.getLayoutParams();
-                layoutParams.height = height;
-                loadingView.setLayoutParams(layoutParams);
-            }
-        }
-    };
 
     /**
      * 显示特定的 View，替换 mView
@@ -73,18 +50,12 @@ public class ViewSwitcher {
                 parent.removeView(view);
             }
             parentView.removeViewAt(viewIndex);
-            // 将 view 覆盖在 mView 上面，这样 view 可以随着 mView 的"可见区域"高度动态变化
-            FrameLayout frameLayout = new FrameLayout(mView.getContext());
             if (view != mView) {
                 if (mView.getParent() != null) {
                     ((ViewGroup) mView.getParent()).removeView(mView);
                 }
-                frameLayout.addView(mView);
-                view.setPadding(0, statusBarHeight, 0, 0);
             }
-            frameLayout.addView(view);
-            params.height = height;
-            parentView.addView(frameLayout, viewIndex, params);
+            parentView.addView(view, viewIndex, params);
         }
     }
 
