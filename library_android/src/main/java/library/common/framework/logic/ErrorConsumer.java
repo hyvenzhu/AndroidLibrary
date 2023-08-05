a88b317dd11ce622702175256bc2eef8d2b0e2e6
hyvenzhu <514126671@qq.com>
2023-08-05 21:45:19 +0800

    [feat] 删除过时的代码

--- library_android/src/main/java/library/common/framework/logic/ErrorConsumer.java
+++
@@ -1,9 +0,0 @@
-package library.common.framework.logic;
-
-/**
- * @author zhuhf
- * @version [AndroidLibrary, 2018-04-20]
- */
-public interface ErrorConsumer<T> {
-    T onError(Throwable throwable);
-}
